4cdb5068026542be80d43936c697974e0320439f
dedaliacatalina <catalina@iMac-de-Catalina.local>
2022-12-19 00:12:45 -0300

    se termina el css

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+let contact_element = document.querySelector("#contact-section")
+function gotocontact(){
+    contact_element.scrollIntoView({behavior: "smooth"});
+}
+
+
+let destacados_element = document.querySelector("#destacados-section")
+function gotocontact(){
+    destacados_element.scrollIntoView({behavior: "smooth"});
+}
+
+let quienessomos_element = document.querySelector("#quienessomos-section")
+function gotocontact(){
+    quienessomos_element.scrollIntoView({behavior: "smooth"});
+}
